3d10f7ec1b65371ed65625533fe47adfbf69ddf1
kingsley idoko <kingsley@gmail.com>
2023-04-03 17:45:11 -0400

    adding route table and internet gateway

--- main.tf
+++ main.tf
@@ -4,13 +4,33 @@ provider "aws" {
 resource "aws_vpc" "myapp_vpc" {
   cidr_block = var.vpc_cidr_block
   tags = {
-    Name: "${var.env_prefix}-vpc"
+    Name: "${var.env_prefix[0]}-vpc"
   }
 }
 resource "aws_subnet" "myapp-subnet-1" {
   vpc_id     = aws_vpc.myapp_vpc.id
   cidr_block =  var.subnet_cidr_block
-  availability_zone = var.avail_zone
+  availability_zone = var.avail_zone[0]
     tags = {
-    "Name" =  "${var.env_prefix}-subnet-1"
+    "Name" =  "${var.env_prefix[0]}-subnet-1"
   }
+}
+
+resource "aws_internet_gateway" "myapp-gw" {
+  vpc_id = aws_vpc.myapp_vpc.id
+tags = {
+    Name = "${var.env_prefix}-myapp-igw"
+  }
+}
+resource "aws_route_table" "dev-rtb" {
+  vpc_id = aws_vpc.myapp_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-gw
+  }
+tags = {
+    Name = "${var.env_prefix}-myapp-rtb"
+  }
+}
+
--- variable.tf
+++ variable.tf
@@ -1,4 +1,21 @@
-variable "vpc_cidr_block" {}
-variable "subnet_cidr_block" {}
-variable "avail_zone" {}
-variable "env_prefix" {}
+variable "vpc_cidr_block" {
+    type = string
+    description = "Entire VPC Subnet"
+    default = "10.0.0.0/16"
+}
+variable "subnet_cidr_block" {
+    type = string
+    description = "Subnet-1 cidr block"
+    default = "10.0.1.0/24"
+}
+variable "avail_zone" {
+    type = list(string)
+    description = "Subnet-1 cidr block"
+    default = ["eu-west-1a", "eu-west-1b"]
+}
+variable "env_prefix" {
+    type = list(string)
+    description = "enviroment description"
+    default = ["Dev", "Prod"]
+}
+
