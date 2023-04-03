625949c77b6f95b7c00fc43b2189e040d0d6baaf
kingsley idoko <kingsley@gmail.com>
2023-04-03 14:45:01 -0400

    creating resource

--- main.tf
+++ main.tf
@@ -2,20 +2,15 @@ provider "aws" {
   region     = "eu-west-1"
 }
 resource "aws_vpc" "myapp_vpc" {
-  cidr_block = var.cidr_blocks[0].cidr_block
+  cidr_block = var.vpc_cidr_block
   tags = {
-    "Name" = var.cidr_blocks[0].name
-    "env_vpc":"Dev"
+    Name: "${var.env_prefix}-vpc"
   }
 }
 resource "aws_subnet" "myapp-subnet-1" {
-  vpc_id     = aws_vpc.KI_development_vpc.id
-  cidr_block =  var.cidr_blocks[1].cidr_block
-  availability_zone = "eu-west-1b"
+  vpc_id     = aws_vpc.myapp_vpc.id
+  cidr_block =  var.subnet_cidr_block
+  availability_zone = var.avail_zone
     tags = {
-    "Name" =  var.cidr_blocks[0].name
+    "Name" =  "${var.env_prefix}-subnet-1"
   }
-}
-data "aws_vpc" "existing-vpc"{
-    default = true
-}
