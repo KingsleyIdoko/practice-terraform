1bc69c856f0dc15064eeb3bd4bfcf9d97a52c34f
kingsley idoko <kingsley@gmail.com>
2023-04-03 23:34:29 -0400

    added subnets and route associates

--- main.tf
+++ main.tf
@@ -9,17 +9,24 @@ resource "aws_vpc" "myapp_vpc" {
 }
 resource "aws_subnet" "myapp-subnet-1" {
   vpc_id     = aws_vpc.myapp_vpc.id
-  cidr_block =  var.subnet_cidr_block
+  cidr_block =  var.subnet_cidr_blocks[0]
   availability_zone = var.avail_zone[0]
     tags = {
     "Name" =  "${var.env_prefix[0]}-subnet-1"
   }
 }
-
+resource "aws_subnet" "myapp-subnet-2" {
+  vpc_id     = aws_vpc.myapp_vpc.id
+  cidr_block =  var.subnet_cidr_blocks[1]
+  availability_zone = var.avail_zone[1]
+    tags = {
+    "Name" =  "${var.env_prefix[0]}-subnet-2"
+  }
+}
 resource "aws_internet_gateway" "myapp-gw" {
   vpc_id = aws_vpc.myapp_vpc.id
 tags = {
-    Name = "${var.env_prefix}-myapp-igw"
+    Name = "${var.env_prefix[0]}-myapp-igw"
   }
 }
 resource "aws_route_table" "dev-rtb" {
@@ -27,10 +34,18 @@ resource "aws_route_table" "dev-rtb" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.myapp-gw
+    gateway_id = aws_internet_gateway.myapp-gw.id
   }
 tags = {
-    Name = "${var.env_prefix}-myapp-rtb"
+    Name = "${var.env_prefix[0]}-myapp-rtb"
   }
 }
 
+resource "aws_route_table_association" "myapp-route-subnet-1-association" {
+  subnet_id      = aws_subnet.myapp-subnet-1.id
+  route_table_id = aws_route_table.dev-rtb.id
+}
+resource "aws_route_table_association" "myapp-route-subnet-2-association" {
+  subnet_id      = aws_subnet.myapp-subnet-2.id
+  route_table_id = aws_route_table.dev-rtb.id
+}
--- variable.tf
+++ variable.tf
@@ -3,10 +3,10 @@ variable "vpc_cidr_block" {
     description = "Entire VPC Subnet"
     default = "10.0.0.0/16"
 }
-variable "subnet_cidr_block" {
-    type = string
-    description = "Subnet-1 cidr block"
-    default = "10.0.1.0/24"
+variable "subnet_cidr_blocks" {
+    type = list(string)
+    description = "Subnets cidr blocks"
+    default = ["10.0.1.0/24","10.0.2.0/24"]
 }
 variable "avail_zone" {
     type = list(string)
